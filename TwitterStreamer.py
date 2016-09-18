bf185f55770e3cb11b72a24f6e9a9f4ccb4ef5b0
ErikTillberg <eatillberg@gmail.com>
2016-09-18 16:49:12 -0400

    fixes

--- TwitterStreamer.py
+++ TwitterStreamer.py
@@ -10,9 +10,11 @@ import json
 
 import threading
 
-import time
+from time import sleep
+import signal
+import sys
 
-#Variables that contains the user credentials to access Twitter API 
+#Variables that contains the user credentials to access Twitter API
 access_token = "307572680-a6YbnrmtRf93c3QSBKWwJIy7t4xpvtUXBAsy60Ds"
 access_token_secret = "ChU5dC8rHj3WGrTnTOpBvBuRksYJimIU1xYkHR3UbezhU"
 consumer_key = "WbUsbhL6bVlnukhWaHevAgDRO"
@@ -26,11 +28,12 @@ keywords = []
 #This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
 
-    def __init__(self, searchTerm):     
+    def __init__(self, searchTerm):
         self.searchTerm = searchTerm
-    
+
     def on_data(self, data):
         tweet = json.loads(data)
+        print data
         collection = db[self.searchTerm]
         collection.insert_one(tweet)
         return True
@@ -38,18 +41,40 @@ class StdOutListener(StreamListener):
     def on_error(self, status):
         print status
 
-shutdown_event = threading.Event()
+class TweetFetcher:
 
-def startTwitterSearch(searchTerm):
-    l = StdOutListener(searchTerm)
-    auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    stream = Stream(auth, l)
-    t = threading.Thread(target=stream.filter, kwargs={'track':[searchTerm]})
-    t.start()
+    fetchers = []
     
-    
+    def __init__(self, searchTerm):
+        TweetFetcher.fetchers.append(self)
+        self.searchTerm = searchTerm
+        
+    def startTwitterSearch(self):
+        l = StdOutListener(self.searchTerm)
+        auth = OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        self.stream = Stream(auth, l)
+        self.stream.filter(track = [self.searchTerm], async = True)
+        
+        #self.t = threading.Thread(target=stream.filter, kwargs={'track':[self.searchTerm], 'async':True})
+        #self.t.start()
+    def stopSearch(self):
+        self.stream.disconnect()
+
 if __name__ == '__main__':
     
-    startTwitterSearch('apple')
-    startTwitterSearch('microsoft')
+    def ctrlCHandler(signal, frame):
+        print 'exiting'
+        for i in range(0, len(TweetFetcher.fetchers)):
+            TweetFetcher.fetchers[i].stopSearch()
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, ctrlCHandler)
+    
+    t = TweetFetcher('microsoft')
+    t.startTwitterSearch()
+    t1 = TweetFetcher('apple')
+    t1.startTwitterSearch()
+
+    while True:
+        sleep(0.1)

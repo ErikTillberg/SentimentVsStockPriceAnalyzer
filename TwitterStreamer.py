1fcaea236bedbc9b7e574c6af9d9535c0f134d35
ErikTillberg <eatillberg@gmail.com>
2016-09-18 15:05:29 -0400

    multithreaded streaming

--- TwitterStreamer.py
+++ TwitterStreamer.py
@@ -8,8 +8,15 @@ from pymongo import MongoClient
 
 import json
 
-#Variables that contains the user credentials to access Twitter API 
+import threading
 
+import time
+
+#Variables that contains the user credentials to access Twitter API 
+access_token = "307572680-a6YbnrmtRf93c3QSBKWwJIy7t4xpvtUXBAsy60Ds"
+access_token_secret = "ChU5dC8rHj3WGrTnTOpBvBuRksYJimIU1xYkHR3UbezhU"
+consumer_key = "WbUsbhL6bVlnukhWaHevAgDRO"
+consumer_secret = "NF97BBUFmqyr6xJUF0aIk4Rnyl0SHNkPkyaDPZSOHIpjCnXZ7Z"
 
 client = MongoClient()
 db = client.TwitterData
@@ -19,23 +26,30 @@ keywords = []
 #This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
 
+    def __init__(self, searchTerm):     
+        self.searchTerm = searchTerm
+    
     def on_data(self, data):
         tweet = json.loads(data)
-        db.tweets.insert_one(tweet)
-        print data
+        collection = db[self.searchTerm]
+        collection.insert_one(tweet)
         return True
 
     def on_error(self, status):
         print status
 
+shutdown_event = threading.Event()
 
-if __name__ == '__main__':
-
-    #This handles Twitter authetification and the connection to Twitter Streaming API
-    l = StdOutListener()
+def startTwitterSearch(searchTerm):
+    l = StdOutListener(searchTerm)
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     stream = Stream(auth, l)
-
-    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-    stream.filter(track=['Tesla', 'Apple', 'Microsoft'])
+    t = threading.Thread(target=stream.filter, kwargs={'track':[searchTerm]})
+    t.start()
+    
+    
+if __name__ == '__main__':
+    
+    startTwitterSearch('apple')
+    startTwitterSearch('microsoft')

faad722e8de7b54886d37c96f11e80c890c373d7
ErikTillberg <eatillberg@gmail.com>
2016-10-02 15:45:44 -0400

    t

--- TwitterStreamer.py
+++ TwitterStreamer.py
@@ -15,10 +15,10 @@ import signal
 import sys
 
 #Variables that contains the user credentials to access Twitter API
-access_token =
+access_token = 
 access_token_secret =
 consumer_key =
-consumer_secret = 
+consumer_secret =
 
 client = MongoClient()
 db = client.TwitterData

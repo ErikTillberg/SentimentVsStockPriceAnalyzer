f7fbb1594bb0ecba9f983664e155c694694d759a
Steven Engler <smengler3@gmail.com>
2016-10-10 20:20:39 -0400

    Now logs uncaught exceptions.
    Any exceptions which are not caught are logged. Exceptions from threads are also caught and logged.

--- TwitterStreamer.py
+++ TwitterStreamer.py
@@ -89,14 +89,55 @@ class TweetFetcher:
         for x in TweetFetcher.fetchers:
             x.stopSearch()
         #
-    
+
+class ExceptionLogger(object):
+    def __init__(self):
+        self._logger = logging.getLogger(__name__)
+    #
+    def handle_exception(self, exc_type, exc_value, exc_traceback):
+        # see here: http://stackoverflow.com/a/16993115/3731982
+        if issubclass(exc_type, KeyboardInterrupt):
+            sys.__excepthook__(exc_type, exc_value, exc_traceback)
+            return
+        #
+        self._logger.error("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
+    #
+
+def _install_thread_excepthook():
+    '''
+    Workaround for sys.excepthook thread bug
+    From http://spyced.blogspot.com/2007/06/workaround-for-sysexcepthook-bug.html (https://sourceforge.net/tracker/?func=detail&atid=105470&aid=1230540&group_id=5470).
+    Call once from __main__ before creating any threads.
+    If using psyco, call psyco.cannotcompile(threading.Thread.run)
+    since this replaces a new-style class method.
+    '''
+    import threading
+    init_old = threading.Thread.__init__
+    def init(self, *args, **kwargs):
+        init_old(self, *args, **kwargs)
+        run_old = self.run
+        def run_with_except_hook(*args, **kw):
+            try:
+                run_old(*args, **kw)
+            except (KeyboardInterrupt, SystemExit):
+                raise
+            except:
+                sys.excepthook(*sys.exc_info())
+        self.run = run_with_except_hook
+    threading.Thread.__init__ = init
+
 if __name__ == '__main__':
+    _install_thread_excepthook()
+    sys.excepthook = ExceptionLogger().handle_exception
+    #
+    ##############
+    #
     root_logger = logging.getLogger()
     root_logger.setLevel(logging.DEBUG)
     #
     import color_stream_handler
     import time
-	#
+    #
     stream_handler = color_stream_handler.ColorStreamHandler()
     stream_handler.setFormatter(logging.Formatter('%(levelname)-6s : %(name)-25s : %(message)s'))
     file_log_handler = logging.FileHandler('twitter - %s.log'%time.strftime("%a, %d %b %Y %Hh%Mm%Ss",time.localtime()))
--- alchemy_sentiment.py
+++ alchemy_sentiment.py
@@ -228,7 +228,49 @@ def get_sentiment_score_for_targets_from_text(text, targets, apikey):
         raise Exception('Could not parse the sentiment score from the result:\n'+str(r.text))
     #
 #
+class ExceptionLogger(object):
+    def __init__(self):
+        self._logger = logging.getLogger(__name__)
+    #
+    def handle_exception(self, exc_type, exc_value, exc_traceback):
+        # see here: http://stackoverflow.com/a/16993115/3731982
+        if issubclass(exc_type, KeyboardInterrupt):
+            sys.__excepthook__(exc_type, exc_value, exc_traceback)
+            return
+        #
+        self._logger.error("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
+    #
+#
+def _install_thread_excepthook():
+    '''
+    Workaround for sys.excepthook thread bug
+    From http://spyced.blogspot.com/2007/06/workaround-for-sysexcepthook-bug.html (https://sourceforge.net/tracker/?func=detail&atid=105470&aid=1230540&group_id=5470).
+    Call once from __main__ before creating any threads.
+    If using psyco, call psyco.cannotcompile(threading.Thread.run)
+    since this replaces a new-style class method.
+    '''
+    import threading
+    init_old = threading.Thread.__init__
+    def init(self, *args, **kwargs):
+        init_old(self, *args, **kwargs)
+        run_old = self.run
+        def run_with_except_hook(*args, **kw):
+            try:
+                run_old(*args, **kw)
+            except (KeyboardInterrupt, SystemExit):
+                raise
+            except:
+                sys.excepthook(*sys.exc_info())
+        self.run = run_with_except_hook
+    threading.Thread.__init__ = init
+#
 if __name__ == '__main__':
+    import sys
+    _install_thread_excepthook()
+    sys.excepthook = ExceptionLogger().handle_exception
+    #
+    ##############
+    #
     root_logger = logging.getLogger()
     root_logger.setLevel(logging.DEBUG)
     #

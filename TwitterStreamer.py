9c623430d4f7c050c0ca2d58be5e9cde52cd2477
Erik Tillberg <eatillberg@gmail.com>
2016-10-12 13:14:11 -0400

    allows for multiple searches

--- TwitterStreamer.py
+++ TwitterStreamer.py
@@ -17,24 +17,34 @@ from time import sleep
 import signal
 import sys
 
+import re
+
 client = MongoClient()
 db = client.TwitterData
 
 #This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
 
-    def __init__(self, searchTerm):
-        self.searchTerm = searchTerm
+    def __init__(self, searchTerms):
+        self.searchTerms = searchTerms
         self._logger = logging.getLogger(__name__)
-    
+
     def on_data(self, data):
         try:
             tweet = json.loads(data)
-            #
-            collection = db[self.searchTerm]
-            collection.insert_one(tweet)
-            #
+            #Search the list of words:
+            matchedWord = self.matchToString(tweet['text'])
+            if (matchedWord != None):
+                collection = db[matchedWord]
+                collection.insert_one(tweet)
+            #if a match wasn't found, the text must have appeared in an imbedded link.
+            else:
+                collection = db['unmatched']
+                collection.insert_one(tweet)
+
+            print matchedWord
             print tweet['text'].encode('utf-8')
+
             #
             '''
             try:
@@ -53,37 +63,53 @@ class StdOutListener(StreamListener):
         finally:
             return True
         #
-    
+
+    #method checks through list of lists of search words, matching each to the string. If it finds a match it returns the
+    #first item in the list it matched to.
+    def matchToString(self, tweetText):
+        #for each group of search terms
+        for l in self.searchTerms:
+            #for each word in that group
+            for word in l:
+                matchObj = re.match(r'(.*)' + word + '(.*?).*', tweetText, re.M|re.I)
+                if (matchObj): #if it matched, return first element in the list.
+                    return l[0]
+        #If it get's here, return None (no match).
+        return None
+
     def on_error(self, status):
         self._logger.error('StreamListener error: %s', status)
 
 class TweetFetcher:
-    
+
     fetchers = []
-    
-    def __init__(self, searchTerm, extraTerms):
+
+    def flatten(self, listOfLists):
+        "Flatten one level of nesting"
+        return [x for sublist in listOfLists for x in sublist]
+
+    def __init__(self, searchTerms):
         TweetFetcher.fetchers.append(self)
-        self.searchTerm = searchTerm
-        self.extraTerms = extraTerms
+        self.searchTerms = searchTerms
         self._logger = logging.getLogger(__name__)
-    
+
     def startTwitterSearch(self):
-        l = StdOutListener(self.searchTerm)
+        l = StdOutListener(self.searchTerms)
         auth = OAuthHandler(consumer_key, consumer_secret)
         auth.set_access_token(access_token, access_token_secret)
+
         self.stream = Stream(auth, l)
-        self.stream.filter(track = ([self.searchTerm]+self.extraTerms), async = True, languages=['en'])#, filter_level='medium')
-        #
-        #self.t = threading.Thread(target=stream.filter, kwargs={'track':[self.searchTerm], 'async':True})
-        #self.t.start()
-        #
+
+        #Flatten the list of search terms, searching for every word.
+        self.stream.filter(track = self.flatten(self.searchTerms), async = False, languages=['en'])#, filter_level='medium')
+
         self._logger.debug('Starting stream.')
-    
+
     def stopSearch(self):
         self.stream.disconnect()
         #
         self._logger.debug('Stopping stream.')
-    
+
     @staticmethod
     def stopAll():
         for x in TweetFetcher.fetchers:
@@ -151,7 +177,7 @@ if __name__ == '__main__':
     ##############
     #
     import yaml
-    with open('api_keys.yaml', 'r') as f:
+    with open('../api_keys.yaml', 'r') as f:
         api_keys = yaml.load(f)
     #
     access_token = api_keys['twitter']['access_token']
@@ -160,17 +186,16 @@ if __name__ == '__main__':
     consumer_secret = api_keys['twitter']['consumer_secret']
     #
     ##############
-    
+
     def ctrlCHandler(signal, frame):
         logger.debug('Exiting program (CTRL-C).')
         TweetFetcher.stopAll()
         sys.exit(0)
-    
+
     signal.signal(signal.SIGINT, ctrlCHandler)
-    
-    t = TweetFetcher('google', ['android', 'GOOGL', 'Alphabet Inc'])
+
+    t = TweetFetcher([['microsoft', 'windows 10', 'xbox'], ['tesla', 'elon musk'], ['dow jones', 'dowj', 'dji'], ['wells fargo'], ['Ford']])
     t.startTwitterSearch()
-    
+
     while True:
         sleep(20)
-    

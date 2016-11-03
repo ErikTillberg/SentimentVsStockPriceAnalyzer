a3e0b8fbc8fd7c3a7116a7f7418847f5c5690a1d
Steven Engler <smengler3@gmail.com>
2016-11-02 20:45:08 -0400

    Changed the SVM to work with new data format.

--- R_Scripts/processing_version_2_svm.r
+++ R_Scripts/processing_version_2_svm.r
@@ -1,32 +1,37 @@
 #Support Vector Machine to analyze stock price & twitter sentiment
 
 #install.packages("e1071")
+library(tree)
+library(rpart)
 library("e1071")
-library(jsonlite)
 
 set.seed(1)
 
 load("twitterStockData_TSLA.rda")
+stock_dat = read.csv("AAPL.csv", header=TRUE)
 
-compound = json_data$sentiment$compound
-positive = json_data$sentiment$positive
-negative = json_data$sentiment$negative
+plot((stock_dat$DATE-stock_dat$DATE[1])/(3600*24), stock_dat$CLOSE)
+plot((json_data$timestamp_sec-json_data$timestamp_sec[1])/(3600*24), json_data$stock_changes$`24`)
 
-stockChange = json_data$stockChange
+total_data = data.frame(
+  sentiment_pos = json_data$sentiment$pos,
+  sentiment_neg = json_data$sentiment$neg,
+  sentiment_neu = json_data$sentiment$neu,
+  sentiment_cmp = json_data$sentiment$compound,
+  #stock_change = json_data$stock_changes$`24`
+  stock_change = ifelse(json_data$stock_changes$`24`>=0, "Yes", "No")
+)
 
-dat = data.frame(c = compound, p=positive,n=negative, s = stockChange)
-
-train = sample(1:nrow(dat),nrow(dat)*0.75)
+train = sample(1:nrow(total_data),nrow(total_data)*0.75)
 test = -train
 
 training_data = total_data[train,]
 test_data = total_data[test,]
 
-testing_result = dat$stockChange[test]
-
-svmfit = svm(testing_result, data = dat[train,], kernel = "linear", cost = 10, scale = FALSE)
+svmfit = svm(stock_change~., data = training_data, kernel = "linear", cost = 10, scale = FALSE)
 summary(svmfit)
 
-#predictions:
 predictions = predict(svmfit, test_data)
 predictions
+
+table(predictions, test_data$stock_change)

7f63f6edbc4b45c828cdcbf010ff709c20f1c150
Steven Engler <smengler3@gmail.com>
2016-11-15 21:38:43 -0500

    Changed 'add_stock_price_change' to module.

--- sentiment_vs_stock/Stock_Price_Manipulation/add_stock_price_change.py
+++ sentiment_vs_stock/Stock_Price_Manipulation/add_stock_price_change.py
@@ -1,104 +1,87 @@
 from pymongo import MongoClient
+
 import math
 import re
 import json
 import time
+import logging
 import numpy as np
-from stock_data import parse_stock_data, get_stock_data, get_stock_table_string
 
-stock_name = 'TSLA'
-collection_name = 'tesla filtered_filtered_no_garbage'
-interval_seconds = 3600
-#
-###############
-#
-response = get_stock_data(interval_seconds, 30, stock_name)
-print response['header']
-print response['data']
-table = parse_stock_data(response['data'], interval_seconds)
-print get_stock_table_string(table)
-print
+from . import stock_data
+from ..mongo_helpers import loop_over_collection
 
-def get_stock_val(timestamp, table, before_or_after):
-	# may throw an exception when using 'after'
-	table_times = [x[0] for x in table]
-	time_offsets = np.array(table_times)-timestamp
-	zero_crossings = np.where(np.diff(np.sign(time_offsets)))[0]
-	# http://stackoverflow.com/a/3843124
+_logger = logging.getLogger(__name__)
+
+def add_stock_price_changes_to_collection(collection_name, stock_name, stock_interval_seconds, log_debugging=False):
+	# adds the stock change in-place
 	#
-	if before_or_after == 'before':
-		add_index_offset = 0
-	elif before_or_after == 'after':
-		add_index_offset = 1
-	else:
-		raise Exception('Must use "before" or "after".')
+	response = stock_data.get_stock_data(stock_interval_seconds, 30, stock_name)
+	if log_debugging:
+		_logger.debug(response['header'])
+		_logger.debug(response['data'])
 	#
-	if len(zero_crossings) == 1:
-		return table[zero_crossings[0]+add_index_offset][1]
-	elif len(zero_crossings) > 1:
-		# for this bug: "This doesn't work when there is a zero in the array. It will detect them twice! Example: a = [2,1,0,-1,2] will give array([1, 2, 3])"
-		return table[zero_crossings[1]+add_index_offset][1]
+	table = stock_data.parse_stock_data(response['data'], response['columns'], stock_interval_seconds)
+	if log_debugging:
+		_logger.debug(stock_data.get_stock_table_string(table['data'], table['columns']))
 	#
-#
-#print table[10][0]+2352
-#print get_stock_val(table[10][0]+2352, table, 'before')
-
-###############
-
-client = MongoClient()
-db = client.TwitterData
-
-
-collection = db[collection_name+'_filtered']
-coll_size = db.command('collstats', collection_name+'_filtered')['count']
-
-start_time = time.time()
-cursor = collection.find()
-counter = 0
-
-for record in cursor:
-	if counter%1000==0:
-		print 'progress: %d/%d tweets (%.2f), %f seconds'%(counter, coll_size, counter/float(coll_size), time.time()-start_time)
+	table = table['data']
 	#
-	counter += 1
-	timestamp = float(record['timestamp_ms'])/1000.0
-	try:
-		stock_changes = {}
+	def get_stock_val(timestamp, table, before_or_after):
+		# may throw an exception when using 'after'
+		table_times = [x[0] for x in table]
+		time_offsets = np.array(table_times)-timestamp
+		zero_crossings = np.where(np.diff(np.sign(time_offsets)))[0]
+		# http://stackoverflow.com/a/3843124
+		#
+		if before_or_after == 'before':
+			add_index_offset = 0
+		elif before_or_after == 'after':
+			add_index_offset = 1
+		else:
+			raise Exception('Must use "before" or "after".')
+		#
+		if len(zero_crossings) == 1:
+			return table[zero_crossings[0]+add_index_offset][1]
+		elif len(zero_crossings) > 1:
+			# for this bug: "This doesn't work when there is a zero in the array. It will detect them twice! Example: a = [2,1,0,-1,2] will give array([1, 2, 3])"
+			return table[zero_crossings[1]+add_index_offset][1]
+		#
+	#
+	def process_record(record, collection, counter):
+		timestamp = float(record['timestamp_ms'])/1000.0
 		try:
-			stock_changes['1'] = get_stock_val(timestamp+3600*1, table, 'after')-get_stock_val(timestamp, table, 'before')
+			time_steps = [1, 2, 12, 24, 48]
+			stock_changes = {}
+			for x in time_steps:
+				try:
+					stock_changes[str(x)] = get_stock_val(timestamp+3600*x, table, 'after')-get_stock_val(timestamp, table, 'before')
+				except Exception as e:
+					_logger.exception(e)
+					stock_changes[str(x)] = None
+				#
+			#
+			collection.update_one(
+					{
+						'_id': record['_id']
+					},
+					{
+						'$set': {
+							'stock_changes': stock_changes
+						}
+					})
 		except Exception as e:
-			print e
-			stock_changes['1'] = None
+			_logger.exception(e)
+			pass
 		#
-		try:
-			stock_changes['2'] = get_stock_val(timestamp+3600*2, table, 'after')-get_stock_val(timestamp, table, 'before')
-		except:
-			stock_changes['2'] = None
-		#
-		try:
-			stock_changes['12'] = get_stock_val(timestamp+3600*12, table, 'after')-get_stock_val(timestamp, table, 'before')
-		except:
-			stock_changes['12'] = None
-		#
-		try:
-			stock_changes['24'] = get_stock_val(timestamp+3600*24, table, 'after')-get_stock_val(timestamp, table, 'before')
-		except:
-			stock_changes['24'] = None
-		#
-		try:
-			stock_changes['48'] = get_stock_val(timestamp+3600*48, table, 'after')-get_stock_val(timestamp, table, 'before')
-		except:
-			stock_changes['48'] = None
-		#
-		collection.update_one({
-		'_id': record['_id']
-		},{
-		'$set': {
-			'stock_changes': stock_changes
-			}
-		})
-	except Exception as e:
-		print e
-		pass
 	#
+	loop_over_collection(collection_name, process_record, 1000)
+#
+if __name__ == '__main__':
+	logging.basicConfig(level=logging.DEBUG)
+	#
+	stock_name = 'WMT'
+	collection_name = 'walmart_filtered'
+	stock_interval_seconds = 3600
+	#
+	add_stock_price_changes_to_collection(collection_name, stock_name, stock_interval_seconds, True)
 #
---
+++ sentiment_vs_stock/mongo_helpers.py
@@ -0,0 +1,28 @@
+from pymongo import MongoClient
+
+import time
+import logging
+
+_logger = logging.getLogger(__name__)
+
+def loop_over_collection(collection_name, process_record_func, log_msg_every_x_tweets=None):
+	client = MongoClient()
+	db = client.TwitterData
+	#
+	collection = db[collection_name]
+	momentary_collection_size = db.command('collstats', collection_name)['count']
+	#
+	cursor = collection.find()
+	#
+	start_time = time.time()
+	counter = 0
+	#
+	for record in cursor:
+		if log_msg_every_x_tweets is not None and counter%log_msg_every_x_tweets == 0:
+			_logger.info('progress: %d/%d tweets (%.2f), %f seconds'%(counter, momentary_collection_size, counter/float(momentary_collection_size), time.time()-start_time))
+		#
+		process_record_func(record, collection, counter)
+		#
+		counter += 1
+	#
+#
